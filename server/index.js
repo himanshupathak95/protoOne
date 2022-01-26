ad25b2bc13857e746df53434d3307b71d0425e58
Himanshu Pathak <himanshupat95@gmail.com>
2022-01-26 23:24:44 +0530

    initial commit - checkOne

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "THESHIELD95",
+    database: "ProtoOne",
+});
+
+app.use(cors()); 
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res)=>{
+
+    const sqlSelect = "SELECT * FROM one";
+    db.query(sqlSelect, (err, result)=>{
+        res.send(result);
+    });
+});
+
+app.post("/api/insert", (req, res)=>{
+
+    const theName = req.body.theName;
+    const theMessage = req.body.theMessage;
+
+    const sqlInsert = "INSERT INTO one (name, message) VALUES (?,?)";
+    db.query(sqlInsert, [theName, theMessage], (err, result)=>{
+        console.log(err);
+    });
+});
+
+app.listen(3001, () => {
+    console.log('running on port 3001');
+});
